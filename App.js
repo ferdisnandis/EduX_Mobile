6d3883776fd3c7c3eeb3115020e5be2213603123
unknown <fe97964@gmail.com>
2020-11-24 22:18:24 -0300

    Login iniciado

--- App.js
+++ App.js
@@ -2,12 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Navegação
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+//Páginas
+import Login from './pages/login'
+import Home from './pages/home'
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+//Navegação pelo menu lateral
+const Autenticado = () => {
+  return(
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={Home} />
+    </Drawer.Navigator>
+  )
+}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Autenticado" component={Autenticado} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
