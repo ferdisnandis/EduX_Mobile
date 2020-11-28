01aa1b78eacf7fa5ed05b125ef65970f38af2719
Dev-Russo <murilorusso10@gmail.com>
2020-11-28 05:39:33 -0300

    Start dos metodos da pagina de Dicas

--- pages/postagens/index.js
+++ pages/postagens/index.js
@@ -2,6 +2,64 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const Postagens = () => {
+    const [texto, setTexto] = useState('');
+    const [dica, setDica] = useState([]);
+    const [curtidas, setCurtidas] = useState('');
+
+    useEffect(() => {
+        Listar();
+    }, []);
+
+    const Listar = () => {
+        fetch(url + 'dica')
+            .then(response => response.json())
+            .then(data => {
+                setDica(data.data);
+                console.log(data.data);
+                // limparCampos();
+            })
+            .catch(err => console.error(err));
+    }
+    const Curtir = (event, idDica) => {
+        event.preventDefault();
+        let formdata = new FormData();
+
+        formdata.append('IdUsuario', localStorage.getItem('idUsuario'));
+        formdata.append('IdDica', idDica);
+        fetch(url + 'curtida', {
+            method: 'POST',
+            body: formdata,
+        })
+            .then(response => {
+                if (response.ok) {
+                    window.location.reload(); 
+                    //arumar isso ^
+                }
+            })
+
+            .catch(err => console.error(err))
+    }
+
+    const Salvar = (event) => {
+        event.preventDefault();
+        let formdata = new FormData();
+
+        formdata.append('IdUsuario', localStorage.getItem('idUsuario'));
+        formdata.append('Texto', texto);
+        fetch(url + 'dica', {
+            method: 'POST',
+            body: formdata,
+        })
+            .then(response => {
+                if (response.ok) {
+                    window.location.reload();
+                    //arumar isso ^
+                }
+            })
+
+            .catch(err => console.error(err))
+    }
+    
     return(
         <View>
             <Text>Postagens</Text>
