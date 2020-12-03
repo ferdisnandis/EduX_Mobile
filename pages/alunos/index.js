0479991616c98da2fbaf8471c80970bab561c22d
Larissa Moreira <larissamoreira.s2003@gmail.com>
2020-12-03 12:55:15 -0200

    Pagina Alunos e Turma finalizado

--- pages/alunos/index.js
+++ pages/alunos/index.js
@@ -1,10 +1,60 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    item: {
+        backgroundColor: '#6BFAA0',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+      },
+      title: {
+        fontSize: '16px',
+      },
+});
+
 const Alunos = () => {
-    return(
-        <View>
-            <Text>Alunos</Text>
+    const [alunos , setAlunos] = useStates([]);
+    
+    useEffect( () => {
+        ListarAlunos()
+    } , [] )
+
+    const ListarAlunos = () => {
+        fetch( url + 'alunos' , {
+            headers: {
+                'Constent-Type' : 'application/json'
+            }
+        } )
+            .then( response => response.json() )
+            .then( data => {
+                setAlunos( data.data );
+                console.log( data.data )
+            } )
+
+            .catch( err => console.error(err) )
+    }
+
+    const Item = ( {descricao} ) => {
+        return (
+            <View style={style.item}>
+                <Text style={style.title}> {descricao} </Text>
+            </View>
+        )
+    };
+    return (
+        <View style={style.container}>
+            <FlatList
+            data={objetivo}
+            renderItem={renderItem}
+            keyExtractor={item => item.Id}
+            />
         </View>
     )
 }
--- pages/turma/index.js
+++ pages/turma/index.js
@@ -1,12 +1,70 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    item: {
+        backgroundColor: '#6BFAA0',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+      },
+      title: {
+        fontSize: '16px',
+      },
+});
 
 const Turma = () => {
-    return(
-        <View>
-            <Text>Turma</Text>
+    const [turma , setObjetivo] = useState ([]) ;
+
+    useEffect( () => {
+        ListarTurmas()
+    } , [] )
+
+    const ListarTurmas = () => {
+        fetch( url + 'turma' , {
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                setTurma( data.data );
+                console.log( data.data );
+            })
+            .catch( err => console.error (err) )
+    }
+
+    const Item = ( { descricao } ) => {
+        return (
+            <View style={style.item}>
+                <Text style={style.title}>{descricao}</Text>
+            </View>
+        )
+    };
+    
+    const renderItem = ({ item }) => {
+        return (
+            <Item descricao={item.descricao} />
+        )
+    };
+
+    return (
+        <View style={style.container}>
+            <FlatList
+            data={turma}
+            renderItem={renderItem}
+            keyExtractor={item => item.Id }
+            />
         </View>
     )
+
 }
 
 export default Turma;
